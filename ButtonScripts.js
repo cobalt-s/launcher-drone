3c95289cee56c8dc69e87e6bc827079b7eba44aa
lvjimmy <lvjimmydf@gmail.com>
2025-06-06 11:52:28 -0700

    I added scripts to send the buttons to the html pages or whatever hi cobalt!

--- ButtonScripts.js
+++ ButtonScripts.js
@@ -1,4 +1,17 @@
-getElementbyId("button1").addEventListener("click", function() {
-    // Code to execute when button1 is clicked
-    console.log("Button 1 clicked!");
+getElementbyId("Avionics Team").addEventListener("click", function() {
+    // Code to enter the Avionics.HTML
+    console.log("AVionics Button Clicked");
+    window.location.href = "Avionics.html";
+}
+
+getElementById("Structures Team").addEventListener("click", function() {
+    // Code to enter the Structures.HTML
+    console.log("Structures Button Clicked");
+    window.location.href = "Structures.html";
+}
+
+getElementById("Launch Team").addEventListener("click", function() {
+    // Code to enter the Propulsion.HTML
+    console.log("Launch Button Clicked");
+    window.location.href = "Launch.html";
 }
